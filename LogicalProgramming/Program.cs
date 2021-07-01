9b56ca7244d4a8cb1e0c6c37f726e6ae9e6fb0c8
unknown <sivaranjanib99@gmail.com>
2021-07-01 20:13:34 +0530

    [siva] printing the elapsed time

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -7,7 +7,23 @@ namespace LogicalProgramming
         static void Main(string[] args)
         { int Choice = 0;
             Console.WriteLine("welcome to LOGICAL PROGRAMMING!");
-           
+            Console.WriteLine("choose the program:");
+            Console.WriteLine("1) Generating coupon:");
+
+
+            Console.WriteLine("Enter your choice");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    CouponNumber.GenerateCoupon();
+                    break;
+                // case 2:
+                //   break;
+                default:
+                    Console.WriteLine("enter correct option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
--- LogicalProgramming/StopWatch.cs
+++ LogicalProgramming/StopWatch.cs
@@ -8,5 +8,49 @@ namespace LogicalProgramming
 {
     class StopWatch
     {
+        
+        public static void ReadInput()
+        {
+          
+
+            //creating object for class stopwatch
+            StopWatch StopWatch = new StopWatch();
+            Console.WriteLine("Enter 1 to start the stopwatch and 0 to stop the stopwatch:");
+
+            //read inpt from user
+            int choice = Convert.ToInt32(Console.ReadLine());
+            int iter = choice;
+
+            //to run stop watch for particular period of time 
+            while (iter == 1)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        StopWatch.Start();
+                        break;
+                    case 0:
+                        StopWatch.Stop();
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("enter 0 to stop or 1 to continue");
+                iter = Convert.ToInt32(Console.ReadLine());
+            }
+
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = StopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+
     }
 }
+    
+
